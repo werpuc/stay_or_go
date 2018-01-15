787534929f3428a4c8989d85096a4dfbdd0f388c
werpuc <ciemnamateria@gmail.com>
2018-01-16 00:41:01 +0100

    start

---
+++ time_after_time/app.R
@@ -0,0 +1,30 @@
+library(shiny)
+library(ggplot2)
+
+data <- read.csv("time.csv", stringsAsFactors = FALSE, header = TRUE)
+data$both <- data$work + data$sport
+
+
+ui <- fluidPage(
+  titlePanel("Time contribution", windowTitle = "Time after time"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("type", "type", choices = c("work", "sport", "both"))
+    ),
+    mainPanel(
+      plotOutput("plot"),
+      br(),
+      tableOutput("stats")
+    )
+  )
+  
+)
+
+server <- function(input, output, session) {
+  output$plot <- renderPlot({
+    ggplot(data, aes(x = date, y = work)) +
+      geom_point()
+  })
+}
+
+shinyApp(ui = ui, server = server)
