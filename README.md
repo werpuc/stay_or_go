5a16011001a4b59b40283aa153a2026e26d358cd
werpuc <ciemnamateria@gmail.com>
2018-01-17 01:00:01 +0100

    chosen_units

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 [Should I Stay or Should I go](https://www.youtube.com/watch?v=BN1WwnEDWAM)
+
+I don't know. Let's do the math!
--- time_after_time/app.R
+++ time_after_time/app.R
@@ -9,21 +9,32 @@ ui <- fluidPage(
   titlePanel("Time contribution", windowTitle = "Time after time"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("type", "type", choices = c("work", "sport", "both"))
+      selectInput("typeInput", "Graph data", choices = c("work", "sport", "both")),
+      radioButtons("timeTypeInput", "Time unit", choices = c("hours", "pomodoros"), selected = "pomodoros")
     ),
     mainPanel(
       plotOutput("plot"),
       br(),
-      tableOutput("stats")
+      tableOutput("stats"),
+      textOutput("txt")
     )
   )
   
 )
 
 server <- function(input, output, session) {
+  
   output$plot <- renderPlot({
-    ggplot(data, aes_string(x = "date", y = input$type)) +
-      geom_point()
+    timeType <- reactive({input$timeTypeInput})
+    if (timeType() == 'hours'){
+      data$work = data$work/2
+      data$sport = data$sport/2
+      data$both = data$both/2
+    }
+    ggplot(data, aes_string(x = "date", y = input$typeInput)) +
+      geom_point() +
+      expand_limits(y = 0) 
+      # scale_y_continuous(name = "Pomodoros done", limits=c(0, 16))
   })
 }
 
