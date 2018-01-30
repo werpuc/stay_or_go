023fffe58178d39bc543edd02ec4dd384c15e24c
werpuc <ciemnamateria@gmail.com>
2018-01-31 00:48:00 +0100

    small_fixes

--- time_after_time/app.R
+++ time_after_time/app.R
@@ -1,13 +1,20 @@
 library(shiny)
 library(ggplot2)
 
+setwd("C:/Users/Weronika/stay_or_go/time_after_time")
+
 data <- read.csv("time.csv", stringsAsFactors = FALSE, header = TRUE)
 data$both <- data$work + data$sport
 data$weekday <- !(as.POSIXlt(data$date)$wday == 0 | as.POSIXlt(data$date)$wday == 6)
 final_date <- '2018-03-17'
 time_difference <- ceiling(as.numeric(difftime(final_date, Sys.Date())))
-mean_worktime <- mean(data$work)
+mean_worktime <- round(mean(data$work))
+shame_days <- as.numeric(count(data[data$work == 0, ]))
+good_days <- as.numeric(count(data[data$work != 0, ]))
+##
 
+
+##
 ui <- fluidPage(
   titlePanel("Time contribution", windowTitle = "Time after time"),
   sidebarLayout(
@@ -16,9 +23,13 @@ ui <- fluidPage(
       radioButtons("timeTypeInput", "Time unit", choices = c("Hours", "Pomodoros"), selected = "Pomodoros"),
       br(),
       h3("Basic stats"),
-      h5("Mean daily worktime : ", mean_worktime, "pomodoros and that's ", mean_worktime/2, " in hours."),
+      h5("Mean daily worktime : ", mean_worktime, "pomodoros and that's ", round(mean_worktime/2), " in hours."),
       br(), 
-      h1("Days left:", time_difference)
+      h1("Days left:", time_difference),
+      h5("Since the beginning : "),
+      h4("Shame days: ", shame_days),
+      h4("Worked days: ", good_days),
+      h4("More work then shame? ", good_days > shame_days)
     ),
     mainPanel(
       plotOutput("plot")
@@ -43,7 +54,8 @@ server <- function(input, output, session) {
       expand_limits(y = 0) +
       labs(title = "Time commitment over days", subtitle = "basic plot") +
       labs(x = "Date") +
-      labs(y = title)
+      labs(y = title) +
+      theme(axis.text.x = element_text(angle = 90, hjust = 1))
       
       # + scale_y_continuous(name = "Pomodoros done", limits=c(0, 16))
   })
