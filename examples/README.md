ab41da9061966db1d2308b23eed08e2b8bf1561a
werpuc <ciemnamateria@gmail.com>
2018-02-07 00:25:47 +0100

    c_changes

--- examples/README.md
+++ examples/README.md
@@ -3,3 +3,9 @@ MODULE A
 module_a_example.R - sentiment analysis example, overall opinion for data
 
 module_a_data.csv - tweets texts containing word 'matka'. to see how they were collected visit [here](https://github.com/werpuc/prawdziwa_dama)
+
+
+
+Data sources:
+[WYCH_2958_CTAB_20180206200238.csv](https://bdl.stat.gov.pl/BDL/dane/podgrup/temat)
+[API_POL_DS2_en_csv_v2.csv](https://data.worldbank.org/country/poland)
--- examples/module_c_example.R
+++ examples/module_c_example.R
@@ -14,5 +14,24 @@ data$year <- as.numeric(as.character(data$year))
 
 ggplot(data, aes(x = year, y = value)) + 
   geom_point() + 
-  geom_smooth()
+  geom_smooth(method = 'lm') +
+  labs(y = 'Odsetek dzieci objetych wychowaniem przedszkolnym', x = 'Rok', title = 'Ogolem dzieci w wieku 3 - 5 lat')
 
+ggsave("module_c_result_image.png")
+
+model <- lm(value ~ year, data)
+
+#y = a*x +b
+a <- as.numeric(coef(model)[2])
+b <- as.numeric(coef(model)[1])
+
+if (a > 1){
+  result <- "Point for Poland."
+} else if (a < 1 & a > -1) {
+  result <- "Zero points."
+} else {
+  result <- "Point from Poland."
+}
+
+print(result)
+# Point for Poland
