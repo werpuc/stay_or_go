5bc416932090bbc6e2e33fb71bb868bfc310a836
werpuc <ciemnamateria@gmail.com>
2018-02-12 02:33:03 +0100

    2017_result_image

--- examples/module_a_2017_result.R
+++ examples/module_a_2017_result.R
@@ -1,11 +1,16 @@
+library(ggplot2)
+library(reshape2)
+
 setwd("C:\\Users\\Weronika\\stay_or_go\\examples")
 data = read.csv("module_a_2017_data.csv", stringsAsFactors=FALSE, header = FALSE, sep = ' ')
 
-colnames(data) <- c("date", "tweets_good", "tweets_bad")
-head(data)
+colnames(data) <- c("date", "good", "bad")
+# data$date <- months(as.Date(data$date))
+data <- melt(data, variable.name = "kind")
 
-ggplot(data, aes(x = date, y = tweets_good)) +
-  geom_point()
+ggplot(data, aes(x = date, y = value, col = kind)) +
+  geom_point() +
+  labs(title = "Tweets ratio (2017)", x = "Date", y = "Ratio") + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-ggplot(data, aes(x = date, y = tweets_bad)) +
-  geom_point()
+ggsave("module_a_2017_result_image.png")
